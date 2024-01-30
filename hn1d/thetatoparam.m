141e9b6d75c8caae726115850ce090efce7cc808
Christian Tezza <65645957+tezzachris@users.noreply.github.com>
2024-01-30 17:30:31 +0100

    Delete hn1d directory

--- hn1d/thetatoparam.m
+++
@@ -1,9 +0,0 @@
-function a = thetatoparam(theta)
-    
-    a(1) = exp(theta(1));
-    a(2) = exp(theta(2))/(1+exp(theta(2)));
-    a(3) = exp(theta(3));
-    a(4) = exp(theta(4))/(1+exp(theta(4)));
-    a(5) = (theta(5));
-
-end
