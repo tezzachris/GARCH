458801361ddaac248f872b90ebf27e18187bea89
Christian Tezza <65645957+tezzachris@users.noreply.github.com>
2024-01-30 17:46:30 +0100

    Create main.m

---
+++ hn1d/main.m
@@ -0,0 +1,20 @@
+
+%Inputs needed:
+%data: vector of log-returns
+%rf: constant risk free rate
+
+%Estimate parameters
+
+start = []; %set empty if no idea about starting paramters
+[a,ll, flag, fo] = fmincon_hn(data, rf, start);  
+
+%Estimate st.errors
+
+nw=0; %No newey west on scores
+estimflag = 0; %not repeat estimation
+[VCVrobust,A,~,scores,hess]=robustvcv('log_likelihood_hn',a,nw,data,estimflag,rf);
+m = 1;
+T = size(data,1);
+VCV=hess^(-1)/(T-m);
+se = sqrt(diag(VCV));
+signif = ( abs( ahn ) ./ se ) >= tinv(0.95, size(data,1)-length(ahn)) ;
