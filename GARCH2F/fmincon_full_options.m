ffaa3c39070563662436aaae74e1055190bc2f19
Christian Tezza <65645957+tezzachris@users.noreply.github.com>
2024-10-21 19:55:05 +0200

    Add files via upload

---
+++ GARCH2F/fmincon_full_options.m
@@ -0,0 +1,56 @@
+
+
+%Numerical contrained optimization for the joint-likelihood of GARCH(1,1) Heston-Nandi 
+
+%Inputs:
+%[ret]:[double Tx1] log returns
+%[data]:[double Nx1] options dataframe
+%[rf]:[double 1x1] risk free rate
+%[prev]:[double px1] initial paramters, p=number of paramters
+
+%Outputs:
+%[a]:[double px1] MLEs
+%[log_lik]:[double 1x1] maximum log-likelihood
+%[exitflag]:[double 1x1] fmincon - optimizer exit flag
+%[fo]:[double 1x1] fmincon - first order condition 
+
+function [a,log_lik,start] = fmincon_full_options(ret, data, rf, prev, err)
+    if isempty(prev)
+       prev = start_full(ret,rf);
+    end
+    options = optimoptions(@fmincon, ...
+                            'Display','off', ...
+                            'Diagnostics','off', ...
+                            'ConstraintTolerance',1e-7, ...
+                            'algorithm','sqp');  %altri metodi interior point
+    Aineq = []; 
+    bineq = [];
+    Aeq=[];
+    beq=[];
+    %lowerbound
+    lb = [ 1e-50,1e-50,1e-50,-1000,1e-50,    1e-50,-1000,1e-50, 1e-50,1e-50,1e-50,1e-50,-100 ];
+    %upperbound
+    ub = [ 0.1,0.1,0.1,+1000,1,    0.1,+1000,1, 0.1,0.1,1,1,     +100 ];
+    %%nonlcon = @(param) nonlincon_full(param);    
+    nonlcon = [];
+    estim_flag = 0;
+    
+    T = size(ret,1);
+    N = size(data,1);
+        
+    if isequal(err , 'vega')    
+        fun=@(theta) (T+N)/(2*T) * loglik_full(theta,ret,estim_flag,rf) ... 
+                   + (T+N)/(2*N) * loglik_option_full(theta,data,estim_flag,rf) ;
+    elseif isequal(err , 'iv')
+        fun=@(theta) (T+N)/(2*T) * loglik_full(theta,ret,estim_flag,rf) ... 
+                   + (T+N)/(2*N) * loglik_option_full_iv(theta,data,estim_flag,rf) ;
+    end
+    start = prev;
+    [a,fval]=fmincon(fun,prev,Aineq,bineq,Aeq,beq,lb,ub,nonlcon,options);
+    log_lik = -fval;
+
+end 
+
+
+
+
---
+++ GARCH2F/loglik_option_full_iv.m
@@ -0,0 +1,96 @@
+
+
+%df is a matrix with options inside
+%serve CF_HN
+
+%IMPLIED VOLATILITY ERRORS
+
+function [val, v, sigma2u, IVRMSE] = loglik_option_full_iv(theta,df,estim_flag,rf) %data is zeta N(0,1) vector Tx1
+
+    if estim_flag == 1
+        a = thetatoparam(theta);
+    elseif estim_flag == 0
+        a = theta;
+    end
+
+N = 4096*2; %grid width
+
+% Real space
+x_min = -7.5; x_max = 7.5; % min and max daily returns grid
+dx=(x_max-x_min)/(N-1);
+x=x_min:dx:x_max;
+
+% Fourier space
+w_max=pi/dx;
+dw=2*w_max/N;
+w=[0:dw:w_max, -w_max+dw:dw:-dw];
+
+CFvet = CF_HN_Full(1i*w, 999, rf, a);
+n = size(df,1);
+CTmod_vet = zeros(n,1); %prezzi opzioni modello
+IV_HNModCall = zeros(n,1); %implied volatility modello
+
+for j = 1:n
+
+    S = df(j,:).SPX;
+    K = df(j,:).Strike;
+    Tt = df(j,:).DTM;
+    dum = df(j,:).FlagCallPut;
+    s = S * exp(x); % Option payoff
+
+    %FST method
+    CFvet_norm = CFvet(Tt-1,:);
+
+    if dum == 1
+            v_call = max(s-K,0);
+            v_call = real(ifft(fft(v_call).*CFvet_norm));
+            
+            % Interpolate option prices
+            CT = interp1(s,v_call,S);
+            CTmod_vet(j) = CT * exp(-rf*Tt);
+
+            if CTmod_vet(j) < 0
+                CTmod_vet(j) = 0;
+            end
+            
+            IV_HNModCall(j) = blsimpv(S , K , rf * 252, Tt/252, CTmod_vet(j)...
+                                        , 'Limit',1,'Yield',0,'Class', {'Call'}); 
+
+       elseif dum == 0
+
+            v_put = max(K-s,0);
+            v_put = real(ifft(fft(v_put).*CFvet_norm));
+
+            %Interpolate option prices
+            CT = interp1(s,v_put,S);
+            CTmod_vet(j) = CT * exp(-rf*Tt);
+
+            if CTmod_vet(j) < 0
+                CTmod_vet(j) = 0;
+            end
+
+            IV_HNModCall(j) = blsimpv(S , K , rf * 252, Tt/252, CTmod_vet(j)...
+                                        , 'Limit',1,'Yield',0,'Class', {'Put'}); 
+                                  
+    end
+
+end
+
+idx = find(CTmod_vet~=0);
+UiCall = (df.IVMarket(idx) - IV_HNModCall(idx))./df.IVMarket(idx);
+sigma2u = var(UiCall,'omitnan');
+v = - 0.5 * ( log(2*pi*sigma2u) + (UiCall).^2 ./ sigma2u );
+
+val = - sum(v,'omitnan'); % log likelihood
+
+IVRMSE = 100*sqrt(mean( (df.IVMarket(idx) - IV_HNModCall(idx)).^2,'omitnan' ));
+
+end 
+
+
+
+%Other errors
+
+%ei =  (df.PrezzoOpzione-CTmod_vet)./df.VegaMarket ;
+%VWRMSE = sqrt(mean(ei.^2,'omitnan'));
+%v = - 0.5 * ( log(2*pi*VWRMSE.^2) + (ei).^2 ./ VWRMSE.^2 );
