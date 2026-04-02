10747d34f8e6982f045583f2f109d248b9622f4f
Christian Tezza <65645957+tezzachris@users.noreply.github.com>
2026-04-02 22:22:08 +0200

    Update main.m

--- GARCH2F/main.m
+++ GARCH2F/main.m
@@ -14,7 +14,7 @@ estim_flag = 0 %flag = 0 fmincon, = 1 fminunc
 
 %Requires: table of option prices, strikes, days to maturity, flag call/put, underlying
 
-[a,ll] = fmincon_hn_options(ret, df, rf );
+[a,ll] = fmincon_full_options(ret, df, rf );
 
 
 % Standard errors
